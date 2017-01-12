35f72912bba638287fa8542b2ad954842dc183b7
Artnod <artnod78@users.noreply.github.com>
2017-01-12 21:29:28 +0100

    Add files via upload

--- libraries/CompteurDate/CompteurDate.h
+++ libraries/CompteurDate/CompteurDate.h
@@ -9,23 +9,22 @@ class CompteurDate
 {
 	public:
 		CompteurDate(int annee = 2017, int mois = 1, int jour = 1);
-		void start(int annee = 2017, int mois = 1, int jour = 1);
+		void	start(int annee = 2017, int mois = 1, int jour = 1);
 		
-		void run(int key);		
+		void	run(int key);		
 		
-		int annee(void);
-		int mois(void);
-		int jour(void);
-		bool isSelect(void);
+		int		annee(void);
+		int		mois(void);
+		int		jour(void);
+		bool	isSelect(void);
 		
-		int selected(void);
+		int		selected(void);
 
 	private:
-		int _annee;
-		int _mois;
-		int _jour;
-		bool _isSelect;
-		
-		int _selected;
+		int		_annee;
+		int		_mois;
+		int		_jour;
+		bool	_isSelect;
+		int		_selected;
 };
 #endif
